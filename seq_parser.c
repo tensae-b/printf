bf765921d3354556142aa938ce8e3e38b4d7abaf
tensae7654 <tensaeb2016@gmail.com>
2023-04-25 04:41:27 -0700

    group project

--- seq_parser.c
+++ seq_parser.c
@@ -1,23 +1,23 @@
 #include "main.h"
 /**
-* sequence_parser - receives the main string and all the necessary parameters
-* @format: string containing all the desired characters.
-* @spec_list: list of all the posible functions
-* @args: list containing all the argumentents passed
-* Return: total count of printed characters
-*/
-int seq_parser(const char *format, form_spec spec_list[], va_list args)
+ * sequence_parser - Receives the main string and all the necessary parameters
+ * @format: A string containing all the desired characters.
+ * @spec_list: A list of all the posible functions.
+ * @args: A list containing all the argumentents passed to the program.
+ * Return: A total count of the characters printed.
+ */
+int sequence_parser(const char *format, f_specifiers spec_list[], va_list args)
 {
 	int i, j, r_val, printed_chars;
 
 	printed_chars = 0;
-	for (i = 0; format[i] != '\0'; i++)
+	for (i = 0; format[i] != '\0'; i++)/* Iterates through the main str*/
 	{
-		if (format[i] == '%')
+		if (format[i] == '%') /*Checks for format specifiers*/
 		{
-			for (j = 0; spec_list[j].op != NULL; j++)
+			for (j = 0; spec_list[j].sym != NULL; j++)
 			{
-				if (format[i + 1] == spec_list[j].op[0])
+				if (format[i + 1] == spec_list[j].sym[0])
 				{
 					r_val = spec_list[j].f(args);
 					if (r_val == -1)
@@ -26,7 +26,7 @@ int seq_parser(const char *format, form_spec spec_list[], va_list args)
 					break;
 				}
 			}
-			if (spec_list[j].op == NULL && format[i + 1] != ' ')
+			if (spec_list[j].sym == NULL && format[i + 1] != ' ')
 			{
 				if (format[i + 1] != '\0')
 				{
@@ -37,14 +37,13 @@ int seq_parser(const char *format, form_spec spec_list[], va_list args)
 				else
 					return (-1);
 			}
-			i = i + 1;
+			i = i + 1; /*Updating i to skip format symbols*/
 		}
 		else
 		{
-			_putchar(format[i]);
+			_putchar(format[i]); /*call the write function*/
 			printed_chars++;
 		}
 	}
-
 	return (printed_chars);
 }
